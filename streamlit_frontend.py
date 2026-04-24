bec00b9cce2ad2350d536af513be10960155d27d
Ayush Mishra <cyberfungus.07@gmail.com>
2026-04-24 21:03:52 +0530

    Added sidebar

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -1,11 +1,48 @@
 import streamlit as st
 from langchain_core.messages import HumanMessage
 from backend import chatbot
+import uuid
 
-CONFIG = {'configurable': {'thread_id': 'thread-1'}}
+#Utility Functions
+def generate_thread_id():
+    thread_id = str(uuid.uuid4())
+    return thread_id
 
+def reset_chat():
+    thread_id = generate_thread_id()
+    st.session_state['thread_id'] = thread_id
+    add_thread(st.session_state['thread_id'])
+    st.session_state['message_history'] = []
+
+def add_thread(thread_id):
+    if thread_id not in st.session_state['chat_thread']:
+        st.session_state['chat_thread'].append(thread_id)
+
+#Session
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
+
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id'] = generate_thread_id()
+
+if 'chat_thread' not in st.session_state:
+    st.session_state['chat_thread'] = []
+
+add_thread(st.session_state['thread_id'])
+
+CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+
+#sidebar UI
+st.sidebar.title('ChitChat-Bot')
+
+if st.sidebar.button("New Chat", key="new_chat_btn"):
+    reset_chat()
+
+
+st.sidebar.text("My Conversation")
+for thread_id in st.session_state['chat_thread']:
+    st.sidebar.button(thread_id)
+# st.sidebar.text(st.session_state['thread_id'])
 
 # Display chat history
 for msg in st.session_state['message_history']:
@@ -23,10 +60,6 @@ if text_input:
 
     with st.chat_message('user'):
         st.text(text_input)
-
-    # Call chatbot
-
-    # Store assistant response
 
     with st.chat_message('assistant'):
 
